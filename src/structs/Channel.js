314b3219035fa793ffcf6b67cc47696fc493aec3
Gideon Lingle <gideon.lingle2023@gmail.com>
2021-09-03 19:36:39 +0000

    Push logo

--- src/structs/Channel.js
+++ src/structs/Channel.js
@@ -1,7 +1,7 @@
 const { types } = require('../util/Constants').Structures.Channel
 
-const TextChannel = require('./TextChannel')
-const VoiceChannel = require('./VoiceChannel')
+// const TextChannel = require('./TextChannel')
+// const VoiceChannel = require('./VoiceChannel')
 
 class Channel {
 	constructor(obj, client) {
@@ -15,28 +15,7 @@ class Channel {
 
 	}
 
-	edit(obj, reason) {
-		return new Promise((resolve,reject) => {
-			this.client.rest.patch(`/channels/${this.id}`, obj, reason)
-			.then(json => {
-				switch(types[json.type].toLowerCase()) {
-					case "guild_text":
-						var c = new TextChannel(json, this.client)
-						this.client.channels.cache.set(this.id, c)
-						this.guild.channels.cache.set(this.id, c)
-						resolve(c)
-					break;
-
-					case "guild_voice": 
-						var c2 = new VoiceChannel(json, this.client)
-						this.client.channels.cache.set(this.id, c2)
-						this.guild.channels.cache.set(this.id, c2)
-						resolve(c2)
-					break;
-				}
-			})
-		})
-	}
+	
 }
 
 module.exports = Channel
--- src/structs/TextChannel.js
+++ src/structs/TextChannel.js
@@ -1,9 +1,34 @@
 const Channel = require('./Channel')
 const Message = require('./Message')
 
+// console.log(Channel)
+
 class TextChannel extends Channel {
 	constructor(obj, client) {
 		super(obj, client)
+	}
+
+	edit(obj, reason) {
+		return new Promise((resolve,reject) => {
+			this.client.rest.patch(`/channels/${this.id}`, obj, reason)
+			.then(json => {
+				switch(types[json.type].toLowerCase()) {
+					case "guild_text":
+						var c = new TextChannel(json, this.client)
+						this.client.channels.cache.set(this.id, c)
+						this.guild.channels.cache.set(this.id, c)
+						resolve(c)
+					break;
+
+					case "guild_voice": 
+						var c2 = new VoiceChannel(json, this.client)
+						this.client.channels.cache.set(this.id, c2)
+						this.guild.channels.cache.set(this.id, c2)
+						resolve(c2)
+					break;
+				}
+			})
+		})
 	}
 
 	send(content) {
